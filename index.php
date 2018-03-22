7caf7d2df182df2e12b2c32ab5f12926af6972ac
Jason <Jasonamc1@hotmail.com>
2018-03-22 19:24:26 -0300

    login done

--- index.php
+++ index.php
@@ -1,21 +1,62 @@
 <?php
 
+if(isset($_POST['submit']))
+{
+   verify();
+} 
+$feedback = "";
+function verify(){
+    
+    $user = $_POST["username"];
+    $pass = $_POST["password"];
+    
+    $config = parse_ini_file("dbinfo.ini");
+    $servername = $config["servername"];
+    $username = $config["username"];
+    $password = "";
+    $dbName = $config["dbName"];
+    $tblNameLogin = $config["tblNameLogin"];
+    
+    $conn = mysqli_connect($servername, $username, $password, $dbName) or die;
+    
+    $sql = "SELECT id, username, password, hash FROM $tblNameLogin";
+    
+    $result = mysqli_query($conn, $sql) or die;
+    
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            if (password_verify($pass, $row['hash']) == true && $user == $row['username']){
+                header("Location: /project1/home");
+                exit;
+            } else{
+                $feedback = "Username or Password incorrect";
+            }
+        }
+    } 
+    $conn->close();
+}
+
+
+
+
 
 
 
 ?>
-<form action="/project1/login">
+<form method="post" action="/project1/">
   
 
   <div class="container">
-    <label for="uname"><b>Username</b></label>
+    <label for="username"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="username" required>
 
-    <label for="psw"><b>Password</b></label>
+    <label for="password"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="password" required>
 
-    <button type="submit">Login</button>
+    <button type="submit" name="submit">Login</button>
     
   </div>
   </div>
+ 
 </form>
+<?php echo $feedback ?>
--- login.php
+++ login.php
@@ -1,27 +1,31 @@
 <?php
 
-$username = $_GET["username"];
-$password = $_GET["password"];
-echo $username . "<br>" . $password;
+    $user = $_GET["username"];
+    $pass = $_GET["password"];
+    
 
-$config = parse_ini_file("dbinfo.ini");
-$servername = $config["servername"];
-$username = $config["username"];
-$password = "";
-$dbName = $config["dbName"];
-$tblName = $config["tblNameLogin"];
+    $config = parse_ini_file("dbinfo.ini");
+    $servername = $config["servername"];
+    $username = $config["username"];
+    $password = "";
+    $dbName = $config["dbName"];
+    $tblNameLogin = $config["tblNameLogin"];
 
-$conn = mysqli_connect($servername, $username, $password, $dbName) or die;
+    $conn = mysqli_connect($servername, $username, $password, $dbName) or die;
 
-$sql = "SELECT id, username, password FROM $tblName";
+    $sql = "SELECT id, username, password, hash FROM $tblNameLogin";
 
-$result = mysqli_query($conn, $sql) or die;
+    $result = mysqli_query($conn, $sql) or die;
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            if (password_verify($pass, $row['hash']) == true && $user == $row['username']){
+                header("Location: /project1/home");
+                exit;
+            }
+        }
+    } else {
+        header("Location: /project1");
+        exit;
     }
-} else {
-    header("Location: /project1/home");
-    exit;
-}
+    $conn->close();
